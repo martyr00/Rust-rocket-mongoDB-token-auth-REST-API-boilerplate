c8975e81e86eb00d2a093b8ef2981edc9ddcaf4d
martyr00 <leonyvb04@gmail.com>
2022-06-06 00:05:02 +0300

    initial commit JWDtoken

--- src/routes/authorization/token/create_token.rs
+++ src/routes/authorization/token/create_token.rs
@@ -1,11 +1,30 @@
+// TODO JWD TOKEN!!!
 use chrono::Utc;
+use serde::{Deserialize, Serialize};
 
-pub fn create_temp_jwt() {
-    let expiration = create_expiration(3600);
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: usize,
 }
 
-pub fn create_jwt() {
+pub fn create_temp_jwt(uid: String) {
+    let expiration = create_expiration(3600);
+
+    let _claims = claims(uid, expiration);
+}
+
+pub fn create_jwt(uid: String) {
     let expiration = create_expiration(3600 * 7);
+
+    let _claims = claims(uid, expiration);
+}
+
+fn claims(uid: String, exp: i64) -> Claims {
+    Claims {
+        sub: uid.to_owned(),
+        exp: exp as usize,
+    }
 }
 
 fn create_expiration(seconds: i64) -> i64 {
