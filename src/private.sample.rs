d01d05be91065af5e563be42e7eade1aec74218d
martyr00 <leonyvb04@gmail.com>
2022-06-24 01:36:50 +0300

    README

--- src/private.sample.rs
+++ src/private.sample.rs
@@ -1,2 +1,3 @@
 pub const JWT_SECRET: &[u8] = b"<YOUR_JWT_SECRET_KEY>";
+pub const REFRESH_JWT_SECRET: &'static str = b"<YOUR_REFRESH_JWT_SECRET_KEY>";
 pub(crate) const URL_DB: &str = "mongodb+srv://<YOUR_LOGIN>:<YOUR_PASSWORD>@cluster0.d5yn0.mongodb.net/<YOUR_DB_NAME>";
